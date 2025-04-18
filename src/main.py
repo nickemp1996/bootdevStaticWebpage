2d726bc41341cea60915d2f33cf328bfb45eea2b
nickemp1996 <nickemp1996@gmail.com>
2025-04-17 19:36:21 -0600

    static site generator working locally

--- src/main.py
+++ src/main.py
@@ -1,11 +1,102 @@
-from textnode import TextType, TextNode
+import os
+import shutil
+from markdown import markdown_to_html_node, extract_title
+
+def read_file_to_string(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            file_content = file.read()
+            return file_content
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Error: File not found at {file_path}")
+    except Exception as e:
+        raise Exception(f"An error occurred: {e}")
+
+def write_string_to_file(string, file_path):
+    try:
+        with open(file_path, 'w') as file:
+            file.write(string)
+    except Exception as e:
+        raise Exception(f"An error occurred: {e}")
+
+def remove_all_files(directory):
+    """Removes all files within a specified directory, but keeps the directory itself."""
+
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f"Failed to delete {file_path}. Reason: {e}")
+
+def copy_files(src, dest):
+	filenames = os.listdir(src)
+	for filename in filenames:
+		file_path = os.path.join(src, filename)
+		if os.path.isfile(file_path):
+			print(f"{file_path} is a file")
+			shutil.copy(file_path, dest)
+		else:
+			print(f"{file_path} is not a file...\nrecurse")
+			head, tail = os.path.split(file_path)
+			copy_path = os.path.join(dest, tail)
+			if not os.path.exists(copy_path):
+				os.mkdir(copy_path)
+			copy_files(file_path, copy_path)
+
+def setup(src, dest):
+	remove_all_files(dest)
+	copy_files(src, dest)
+
+def generate_page_recursive(from_path, template_path, dest_path):
+	filenames = os.listdir(from_path)
+	for filename in filenames:
+		if filename != ".DS_Store":
+			print(filename)
+			file_path = os.path.join(from_path, filename)
+			print(file_path)
+			if os.path.isfile(file_path):
+				if ".md" in filename:
+					dest_file_path = os.path.join(dest_path, filename.replace(".md", ".html"))
+					print(f"Generating page from {file_path} to {dest_file_path} using {template_path}")
+					try:
+						markdown = read_file_to_string(file_path)
+					except FileNotFoundError:
+						raise FileNotFoundError(f"Error: File not found at {file_path}")
+					except Exception as e:
+						raise Exception(f"An error occurred: {e}")
+
+					try:
+						template = read_file_to_string(template_path)
+					except FileNotFoundError:
+						raise FileNotFoundError(f"Error: File not found at {template_path}")
+					except Exception as e:
+						raise Exception(f"An error occurred: {e}")
+
+					html = markdown_to_html_node(markdown).to_html()
+					title = extract_title(markdown)
+
+					template = template.replace("{{ Title }}", title)
+					template = template.replace("{{ Content }}", html)
+
+					#os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+					try:
+						write_string_to_file(template, dest_file_path)
+					except Exception as e:
+						raise Exception(f"An error occurred: {e}")
+			else:
+				head, tail = os.path.split(file_path)
+				copy_path = os.path.join(dest_path, tail)
+				if not os.path.exists(copy_path):
+					os.mkdir(copy_path)
+				generate_page_recursive(file_path, template_path, copy_path)
 
 def main():
-	text = "this is some anchor text"
-	text_type = TextType.LINK
-	url = "https://www.boot.dev"
-	test = TextNode(text, text_type, url)
-	print(test)
+	setup("static", "public")
+	generate_page_recursive("content/", "template.html", "public/")
 
 if __name__ == "__main__":
 	main()
