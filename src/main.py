14df0b685a99a6afcc1ce27c9515740ace86cbc5
nickemp1996 <nickemp1996@gmail.com>
2025-04-17 20:38:43 -0600

    getting ready to deploy static webpage with github

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 from markdown import markdown_to_html_node, extract_title
 
 def read_file_to_string(file_path):
@@ -51,52 +52,58 @@ def setup(src, dest):
 	remove_all_files(dest)
 	copy_files(src, dest)
 
-def generate_page_recursive(from_path, template_path, dest_path):
+def generate_page(file_path, template_path, dest_file_path, basepath):
+	print(f"Generating page from {file_path} to {dest_file_path} using {template_path}")
+	try:
+		markdown = read_file_to_string(file_path)
+	except FileNotFoundError:
+		raise FileNotFoundError(f"Error: File not found at {file_path}")
+	except Exception as e:
+		raise Exception(f"An error occurred: {e}")
+
+	try:
+		template = read_file_to_string(template_path)
+	except FileNotFoundError:
+		raise FileNotFoundError(f"Error: File not found at {template_path}")
+	except Exception as e:
+		raise Exception(f"An error occurred: {e}")
+
+	html = markdown_to_html_node(markdown).to_html()
+	title = extract_title(markdown)
+
+	template = template.replace("{{ Title }}", title)
+	template = template.replace("{{ Content }}", html)
+	template = template.replace('href="/', f'href="{basepath}')
+	template = template.replace('src="/', f'src="{basepath}')
+
+	try:
+		write_string_to_file(template, dest_file_path)
+	except Exception as e:
+		raise Exception(f"An error occurred: {e}")
+
+def generate_page_recursive(from_path, template_path, dest_path, basepath):
 	filenames = os.listdir(from_path)
 	for filename in filenames:
 		if filename != ".DS_Store":
-			print(filename)
 			file_path = os.path.join(from_path, filename)
-			print(file_path)
 			if os.path.isfile(file_path):
 				if ".md" in filename:
 					dest_file_path = os.path.join(dest_path, filename.replace(".md", ".html"))
-					print(f"Generating page from {file_path} to {dest_file_path} using {template_path}")
-					try:
-						markdown = read_file_to_string(file_path)
-					except FileNotFoundError:
-						raise FileNotFoundError(f"Error: File not found at {file_path}")
-					except Exception as e:
-						raise Exception(f"An error occurred: {e}")
-
-					try:
-						template = read_file_to_string(template_path)
-					except FileNotFoundError:
-						raise FileNotFoundError(f"Error: File not found at {template_path}")
-					except Exception as e:
-						raise Exception(f"An error occurred: {e}")
-
-					html = markdown_to_html_node(markdown).to_html()
-					title = extract_title(markdown)
-
-					template = template.replace("{{ Title }}", title)
-					template = template.replace("{{ Content }}", html)
-
-					#os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-					try:
-						write_string_to_file(template, dest_file_path)
-					except Exception as e:
-						raise Exception(f"An error occurred: {e}")
+					generate_page(file_path, template_path, dest_file_path, basepath)
 			else:
 				head, tail = os.path.split(file_path)
 				copy_path = os.path.join(dest_path, tail)
 				if not os.path.exists(copy_path):
 					os.mkdir(copy_path)
-				generate_page_recursive(file_path, template_path, copy_path)
+				generate_page_recursive(file_path, template_path, copy_path, basepath)
 
 def main():
-	setup("static", "public")
-	generate_page_recursive("content/", "template.html", "public/")
+	if sys.argv[0] == "":
+		basepath = "/"
+	else:
+		basepath = sys.argv[0]
+	setup("static", "docs")
+	generate_page_recursive("content/", "template.html", "docs/", basepath)
 
 if __name__ == "__main__":
 	main()
